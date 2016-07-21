5b17fa7cffe8795c956ec271da87369c5e069b8c
Philippe Gerum <rpm@xenomai.org>
2016-07-21 08:57:40 +0200

    mm: irq_pipeline: lock out oob stage while updating PTE range
    In the non-numa case, change_pte_range() will mark each updated PTE in
    a range as absent, while the protbits are being fixed up, to make sure
    it won't race with the hardware.
    
    We don't want any oob code to preempt the in-band context until the
    updated protbits are written back, so we have to hard disable IRQs
    around this section.

--- mm/mprotect.c
+++ mm/mprotect.c
@@ -42,7 +42,7 @@ static unsigned long change_pte_range(struct vm_area_struct *vma, pmd_t *pmd,
 	struct mm_struct *mm = vma->vm_mm;
 	pte_t *pte, oldpte;
 	spinlock_t *ptl;
-	unsigned long pages = 0;
+	unsigned long pages = 0, flags;
 	int target_node = NUMA_NO_NODE;
 
 	/*
@@ -110,6 +110,7 @@ static unsigned long change_pte_range(struct vm_area_struct *vma, pmd_t *pmd,
 					continue;
 			}
 
+			flags = hard_local_irq_save();
 			oldpte = ptep_modify_prot_start(vma, addr, pte);
 			ptent = pte_modify(oldpte, newprot);
 			if (preserve_write)
@@ -122,6 +123,7 @@ static unsigned long change_pte_range(struct vm_area_struct *vma, pmd_t *pmd,
 				ptent = pte_mkwrite(ptent);
 			}
 			ptep_modify_prot_commit(vma, addr, pte, oldpte, ptent);
+			hard_local_irq_restore(flags);
 			pages++;
 		} else if (IS_ENABLED(CONFIG_MIGRATION)) {
 			swp_entry_t entry = pte_to_swp_entry(oldpte);
