827cd43eace8cf5e6c70f8ad83940d0ad93efd1e
Philippe Gerum <rpm@xenomai.org>
2017-04-22 20:52:59 +0200

    notifier: dovetail: enable atomic notifier from oob stage

--- kernel/notifier.c
+++ kernel/notifier.c
@@ -190,6 +190,9 @@ NOKPROBE_SYMBOL(__atomic_notifier_call_chain);
 int atomic_notifier_call_chain(struct atomic_notifier_head *nh,
 			       unsigned long val, void *v)
 {
+	if (!running_inband())
+		return notifier_call_chain(&nh->head, val, v, -1, NULL);
+
 	return __atomic_notifier_call_chain(nh, val, v, -1, NULL);
 }
 EXPORT_SYMBOL_GPL(atomic_notifier_call_chain);
